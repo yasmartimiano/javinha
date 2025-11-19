9a3c07b2504f6e02c4ce82d984f6f36b4782d5ae
yasmartimiano <yasminliver56@gmail.com>
2025-11-19 12:16:34 -0300

    feat: added a program that says hello to the entered username

---
+++ javinha/src/main/java/introducao01/HelloUser.java
@@ -0,0 +1,14 @@
+package introducao01;
+
+import java.util.Scanner;
+
+public class HelloUser {
+    public static void main(String[] args) {
+        String userName;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please, enter ur name: ");
+        userName = sc.nextLine();
+        System.out.println("Hello, " + userName + "!");
+        sc.close();
+    }
+}

d2473597cab3d2499dab704dd77a0209740148a9
yasmartimiano <yasminliver56@gmail.com>
2025-11-27 15:13:17 -0300

    feat: adding ascii 67

---
+++ javinha/src/main/java/introduction/numbers/ascii67.java
@@ -0,0 +1,22 @@
+package introduction.numbers;
+
+public class ascii67 {
+    public static void main(String[] args) {
+
+        String banner =
+                "      XXXXXXXXXX      XXXXXXXXX XXXXXXXXX\n" +
+                "   XXX               XX              XXX\n" +
+                " XX                   XX           XXX\n" +
+                " XX                     XX        XXX\n" +
+                " XX                       X       XX\n" +
+                " X                               XX\n" +
+                " X    XXXXXXXXXX                XX\n" +
+                "  XX  XX        XX             XX\n" +
+                "  XX X           X            XX\n" +
+                "    XX           XX          XX\n" +
+                "    XXX        XX           XX\n" +
+                "      XXXXXXXXXX           XX";
+
+        System.out.println(banner);
+    }
+}

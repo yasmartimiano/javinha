dae8488d8b4e5cd5362ef36d29c768af93bc5acd
yasmartimiano <yasminliver56@gmail.com>
2025-11-27 14:38:44 -0300

    feat: added the first number file example

---
+++ javinha/src/main/java/introduction/numbers/firstExample.java
@@ -0,0 +1,9 @@
+package introduction.numbers;
+
+public class firstExample {
+public static void main(String[] args) {
+    System.out.println(3);
+    System.out.println(358);
+    System.out.println(50000);
+}
+}

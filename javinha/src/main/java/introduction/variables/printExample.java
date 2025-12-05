7ad96216fdb5ec50cf670ece1d7026abd393fd92
yasmartimiano <yasminliver56@gmail.com>
2025-12-04 23:30:04 -0300

    feat: add a print example

---
+++ javinha/src/main/java/introduction/variables/printExample.java
@@ -0,0 +1,11 @@
+package introduction.variables;
+
+public class printExample {
+
+    public static void main(String[] args) {
+        String firstName = "Caio";
+        String lastName = "Ocean";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+    }
+}
